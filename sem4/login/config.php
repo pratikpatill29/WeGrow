7e9ea1eb2009a6d37c7991b035f29def75db530f
pratikpatill29 <pratikpatil291102@gmail.com>
2024-04-11 22:58:25 +0530

    update the RDS endpoint

--- sem4/login/config.php
+++ sem4/login/config.php
@@ -3,9 +3,9 @@
 This file contains database configuration assuming you are running mysql using user "root" and password ""
 */
 
-define('DB_SERVER', 'php-database.cteudtun1uyx.us-east-1.rds.amazonaws.com');
+define('DB_SERVER', 'ccl-db.cteudtun1uyx.us-east-1.rds.amazonaws.com');
 define('DB_USERNAME', 'admin');
-define('DB_PASSWORD', '12345678');
+define('DB_PASSWORD', '123123123');
 define('DB_NAME', 'login');
 
 // Try connecting to the Database
