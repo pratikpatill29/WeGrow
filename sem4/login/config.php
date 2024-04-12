18fdd297364cf74e7e5c8f0cae133e7fc3c95f6d
pratikpatill29 <pratikpatil291102@gmail.com>
2024-04-12 10:38:50 +0530

    chea

--- sem4/login/config.php
+++ sem4/login/config.php
@@ -6,7 +6,7 @@ This file contains database configuration assuming you are running mysql using u
 define('DB_SERVER', 'ccl-db.cteudtun1uyx.us-east-1.rds.amazonaws.com');
 define('DB_USERNAME', 'admin');
 define('DB_PASSWORD', '123123123');
-define('DB_NAME', 'login');
+define('DB_NAME', 'login2');
 
 // Try connecting to the Database
 $conn = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
